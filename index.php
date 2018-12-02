4f071491a007fd90ce013f19f775e88ab6456cec
Edson Fernandes <edlivelifehd1z@gmail.com>
2018-12-02 16:08:20 -0200

    DAO COMPLETO

--- index.php
+++ index.php
@@ -7,26 +7,43 @@ require_once("config.php");
 //$root->loadById(3);
 //echo $root;
 
+
 //Carrega uma lista de usuários
 //$lista = Usuario::getList();
 //echo json_encode($lista);
+
 
 //Carrega uma lista de usuários buscando pelo login
 //$search = Usuario::search("Ed");
 //echo json_encode($search);
 
-//Carrega um usuário usando login e senha 
-$usuario = new Usuario();
-$usuario->Login("Edson", "123456789");
-echo $usuario;
+
+//Carrega um usuário usando login e senha
+//$usuario = new Usuario();
+//$usuario->Login("Edson", "123456789");
+//echo $usuario;
 
 
+//Criando um novo usuario
+//$aluno = new Usuario("aluno", "@alun0");
+//$aluno->setDeslogin("aluno");
+//$aluno->setDessenha("@alu0");
+//$aluno->insert();
+//echo $aluno;
 
 
+// Alterar um usuario
+//$usuario = new Usuario();
+//$usuario->loadById(8);
+//$usuario->update("professor", "123456");
+//echo $usuario;
 
 
-
-
+// Excluir Usuario
+//$usuario = new Usuario();
+//$usuario->loadById(9);
+//$usuario->delete();
+//echo $usuario;
 
 
 
