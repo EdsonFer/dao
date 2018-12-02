544c04f3624f66b8f5b5aa7278db6993bc535a91
Edson Fernandes <edlivelifehd1z@gmail.com>
2018-12-02 15:12:12 -0200

    third

--- index.php
+++ index.php
@@ -2,29 +2,38 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//Carrega um usuário
+//$root = new Usuario();
+//$root->loadById(3);
+//echo $root;
 
-$root->loadById(3);
+//Carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//Carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("Ed");
+//echo json_encode($search);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Carrega um usuário usando login e senha
+$usuario = new Usuario();
+$usuario->Login("Edson", "123456789");
+echo $usuario;
 
 
 
-/* $sql = new Sql();
+
+
+
+
+
+
+
+
+
+
+
+/*  $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
